215cdb822d221297d40051e3e148b7782e98857b
Graeme <grassenti@gmail.com>
2020-12-01 15:59:08 -0500

    Animated title to support multiple video files as game progresses

--- Plugins/Gimmer_AnimatedTitle/Gimmer_AnimatedTitle.js
+++ Plugins/Gimmer_AnimatedTitle/Gimmer_AnimatedTitle.js
@@ -19,32 +19,87 @@ Gimmer_Core['AnimatedTitle'] = {'loaded':true};
  *
  * @param Movie Path
  * @parent ---Parameters---
- * @type String
+ * @type string
  * @desc Path to the movie that you want to play (starting with movies/, can be in a folder if you want)
+ *
+ * @param ---Changing Titles---
+ * @default
+ *
+ * @param Change Title As Game Progresses
+ * @parent ---Changing Titles---
+ * @type Boolean
+ * @desc Change the title video as the game progress?
  * Default: False
  * @default false
+ *
+ * @param Game Progress Variable
+ * @parent ---Changing Titles---
+ * @desc What variable will track what video to show?
+ * @type variable
+ *
+ * @param List Of Video Files
+ * @parent ---Changing Titles---
+ * @type text[]
+ * @desc A list of video files that you want, in the format of "movies/NameOfFile". Whatever the value of the variable chosen above is the position in this list (starting at 1) that will be played.
+ *
  */
 
+//Parameter handling
 var atParameters = PluginManager.parameters('Gimmer_AnimatedTitle');
-
-
 Gimmer_Core.AnimatedTitle.titleVideoPath = atParameters['Movie Path'];
+Gimmer_Core.AnimatedTitle.ChangeTitleAsGameProgresses = (atParameters['Change Title As Game Progresses'] === "true");
+if(Gimmer_Core.AnimatedTitle.ChangeTitleAsGameProgresses){
+    Gimmer_Core.AnimatedTitle.GameProgressVariable = Number(atParameters['Game Progress Variable']);
+    Gimmer_Core.AnimatedTitle.GameProgressVideoList = eval(atParameters['List Of Video Files']);
+}
+
+//Track flags
 Gimmer_Core.AnimatedTitle.playingTitle = false;
 
+//Helper function to determien video files
+Gimmer_Core.AnimatedTitle.determineVideoFile = function(){
+    let videoFile = "";
+    if(Gimmer_Core.AnimatedTitle.ChangeTitleAsGameProgresses && Gimmer_Core.AnimatedTitle.GameProgressVariable > 0){
+        var json = StorageManager.load(DataManager.latestSavefileId());
+        if(json){
+            json = JsonEx.parse(json);
+            let variables = json.variables;
+            if(variables.value(Gimmer_Core.AnimatedTitle.GameProgressVariable) === 0){
+                variables.setValue(Gimmer_Core.AnimatedTitle.GameProgressVariable,1);
+            }
+            if(variables.value(Gimmer_Core.AnimatedTitle.GameProgressVariable) > 0){
+                videoFile = Gimmer_Core.AnimatedTitle.GameProgressVideoList[variables.value(Gimmer_Core.AnimatedTitle.GameProgressVariable) - 1];
+            }
+            else{
+                videoFile = Gimmer_Core.AnimatedTitle.titleVideoPath;
+            }
+        }
+        else{
+            videoFile = Gimmer_Core.AnimatedTitle.titleVideoPath;
+        }
+    }
+    else{
+        videoFile = Gimmer_Core.AnimatedTitle.titleVideoPath;
+    }
+    return videoFile;
+}
+
+//When you create Scene_Title's background, hijack the GameVideo element and use it for your sinister ends.
 Gimmer_Core.AnimatedTitle._Scene_Title_prototype_createBackground = Scene_Title.prototype.createBackground;
 Scene_Title.prototype.createBackground = function(){
     Gimmer_Core.AnimatedTitle._Scene_Title_prototype_createBackground.call(this);
-    if(Gimmer_Core.AnimatedTitle.titleVideoPath.length){
+    if(Gimmer_Core.AnimatedTitle.determineVideoFile().length){
         Gimmer_Core.AnimatedTitle.videoObject = document.getElementById('GameVideo');
         Gimmer_Core.AnimatedTitle.videoObject.style.opacity = 1;
         Gimmer_Core.AnimatedTitle.videoObject.style.zIndex = 0;
         Gimmer_Core.AnimatedTitle.videoObject.autoplay = true;
         Gimmer_Core.AnimatedTitle.videoObject.loop = true;
-        Gimmer_Core.AnimatedTitle.videoObject.src = Gimmer_Core.AnimatedTitle.titleVideoPath + $gameMap._interpreter.videoFileExt();
+        Gimmer_Core.AnimatedTitle.videoObject.src =  Gimmer_Core.AnimatedTitle.determineVideoFile() + $gameMap._interpreter.videoFileExt();
         Gimmer_Core.AnimatedTitle.playingTitle = true;
     }
 }
 
+//when you leave Scene_Title, stop the video and return it's index back to normal
 Gimmer_Core.AnimatedTitle._Scene_Title_prototype_terminate = Scene_Title.prototype.terminate;
 Scene_Title.prototype.terminate = function(){
     Gimmer_Core.AnimatedTitle._Scene_Title_prototype_terminate.call(this);
@@ -58,6 +113,7 @@ Scene_Title.prototype.terminate = function(){
     }
 }
 
+//Show the video change, and you aren't currently playing an animated title, return it's z-index back to normal
 Graphics._updateVideo = function() {
     this._video.width = this._width;
     this._video.height = this._height;
@@ -67,6 +123,7 @@ Graphics._updateVideo = function() {
     this._centerElement(this._video);
 };
 
+//Make the renderer transparent, this is needed or otherwise the canvas will be opaque by default
 Graphics._createRenderer = function() {
     PIXI.dontSayHello = true;
     var width = this._width;
--- Plugins/Gimmer_AnimatedTitle/README.md
+++ Plugins/Gimmer_AnimatedTitle/README.md
@@ -3,13 +3,23 @@
 Allows you to animate your Title Scene by using a video background
 
 #### Text Tutorial:
-
+#####Static Video:
 Put in the path to the movie that you want on the title screen in the plugin parameters, 
 prefixed with movie/, and without an extension.
 
 Set your title screen image to be a transparent png with your logo.
 
 Watch a lovely video play!
+
+#####Dynamic Video as Game Progresses
+* Set "Change Title As Game Progresses" to true
+* Set "Game Progress Variable" to the variable you are going to track progress with.
+* Set "List Of Video Files" to a list of "movies/firstMove", "movies/secondMovie", etc.
+* Update the Game Progress Variable with the number of movie that you want playing at the intro screen during that part of the game.
+
+**Note**: An uninitialized variable is read as the value of 1, and the first video will play.
+
+As the game progresses, the Title Screen will show the video associated with the variable value set in the most recently saved file. 
 
 
 ## Terms of Use:
