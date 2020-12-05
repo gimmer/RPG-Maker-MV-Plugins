3a4b85e227dccba930c0d08078072e2651f40536
Graeme <grassenti@gmail.com>
2020-12-05 10:58:02 -0500

    ActorSpeed plugin

---
+++ Plugins/Gimmer_ActorSpeed/Gimmer_ActorSpeed.js
@@ -0,0 +1,65 @@
+if(Gimmer_Core === undefined){
+    throw "Gimmer_Core is required for this plugin";
+}
+
+Gimmer_Core['ActorSpeed'] = {'loaded':true};
+
+//=============================================================================
+/*:
+ * @plugindesc Control the actor speed more finely in the game;
+ * @author Gimmer
+ *
+ * @param ---Parameters---
+ * @default
+ *
+ * @param Actor Speed
+ * @parent ---Parameters---
+ * @type number
+ * @min 3
+ * @max 6
+ * @desc Adjusts the movement of the main party
+ * Default: 3.75
+ * @default 3.75
+ *
+ * @param Return Actor Speed
+ * @parent ---Parameters---
+ * @type boolean
+ * @desc After an event moves the player along a route at a specific speed, return to the default speed without requiring another event line
+ * Default: False
+ * @default false
+ *
+ */
+
+var gActorSpeedParams = PluginManager.parameters('Gimmer_ActorSpeed');
+Gimmer_Core.ActorSpeed.ActorSpeed = parseFloat(gActorSpeedParams['Actor Speed']);
+Gimmer_Core.ActorSpeed.ReturnToOriginalSpeed = (gActorSpeedParams === "true");
+
+//Set the movement at initialization of the Game Player.
+Gimmer_Core.ActorSpeed._Game_Player_prototype_initialize = Game_Player.prototype.initialize;
+Game_Player.prototype.initialize = function() {
+    Gimmer_Core.ActorSpeed._Game_Player_prototype_initialize.call(this);
+    this.setMoveSpeed(Gimmer_Core.ActorSpeed.ActorSpeed);
+};
+
+
+if(Gimmer_Core.ActorSpeed.ReturnToOriginalSpeed){
+//Movement should reset to previously set movement after an event with movement that specifies a speed
+    Gimmer_Core.ActorSpeed._Game_Character_initMembers_prototype = Game_Character.prototype.initMembers
+    Game_Character.prototype.initMembers = function() {
+        Gimmer_Core.ActorSpeed._Game_Character_initMembers_prototype.call(this);
+        this._originalMoveSpeed = 0;
+    };
+
+    Gimmer_Core.ActorSpeed._Game_Character_memorizeMoveRoute_prototype = Game_Character.prototype.memorizeMoveRoute
+    Game_Character.prototype.memorizeMoveRoute = function() {
+        Gimmer_Core.ActorSpeed._Game_Character_memorizeMoveRoute_prototype.call(this);
+        this._originalMoveSpeed = this._moveSpeed;
+    };
+
+    Gimmer_Core.ActorSpeed.Game_Character_restoreMoveRoute_prototype = Game_Character.prototype.restoreMoveRoute;
+    Game_Character.prototype.restoreMoveRoute = function() {
+        Gimmer_Core.ActorSpeed.Game_Character_restoreMoveRoute_prototype.call(this);
+        this._moveSpeed = this._originalMoveSpeed;
+        this._originalMoveSpeed  = 0;
+    };
+}
---
+++ Plugins/Gimmer_ActorSpeed/README.md
@@ -0,0 +1,15 @@
+## Gimmer_ActorSpeed
+
+Allows you to set your actor speed to a decimal value.
+
+#### Text Tutorial:
+
+Set the "Actor Speed" parameter to any number you want (switch the input type to "text" if you want to use a fraction)
+
+Optionally, toggle "Return Actor Speed" to true if you want your player to move back to this speed after any move routes in events without you having to specify so manually
+
+## Terms of Use:
+
+Free for both commercial and non-commercial use, with credit.
+
+More Gimmer_ plugins at: https://github.com/gimmer/RPG-Maker-MV-Plugins
