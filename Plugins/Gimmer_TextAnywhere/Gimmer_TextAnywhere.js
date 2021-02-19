587f63eb9a434584e13329ae08cee52277b27f6a
Graeme <grassenti@gmail.com>
2021-02-19 14:57:36 -0500

    Text anywhere

---
+++ Plugins/Gimmer_TextAnywhere/Gimmer_TextAnywhere.js
@@ -0,0 +1,115 @@
+if(Gimmer_Core === undefined){
+    throw "Gimmer_Core is required for this plugin";
+}
+
+//=============================================================================
+/*:
+ * @plugindesc Display text anywhere on the screen
+ * @author Gimmer
+ * @help You can use this plugin to show text on the screen
+ *
+ * Plugin Commands:
+ *
+ * HideTextLayer: Hides the layer all text is on
+ * ShowTextLayer: Shows the layer all text is on
+ *
+ * @param ---Parameters---
+ * @default
+ *
+ * @param Text List
+ * @parent --Parameters--
+ * @type struct<Text>[]
+ * @desc Make a list of all the text you want to show
+ *
+ */
+/*~struct~Text:
+ * @param x
+ * @type String
+ * @desc Where on the x to put the text?
+ * @default 0
+ * Default 0
+ *
+ * @param color
+ * @type string
+ * @desc hexcode of the label text color eg #FF0000
+ *
+ * @param fontsize
+ * @type number
+ * @desc Font size
+ * @default 28
+ * Default Default Font size
+ *
+ * @param bold
+ * @type Boolean
+ * @desc Should the font be bold?
+ * Default false
+ * @default false
+ *
+ * @param y
+ * @type String
+ * @desc Where on the y to put the texT?
+ * @default 0
+ * Default 0
+ *
+ * @param text
+ * @type String
+ * @string What's the text you want (us \V[1] for variables);
+*/
+
+Imported = Imported || {};
+
+Gimmer_Core['TextAnywhere'] = {'loaded':true};
+
+let texts = JSON.parse(PluginManager.parameters('Gimmer_TextAnywhere')['Text List']);
+texts.forEach(function(text, key){
+    text = JSON.parse(text);
+    text.fontsize = Number(text.fontsize);
+    text.bold = (text.bold === "true");
+    texts[key] = text;
+})
+
+Gimmer_Core.TextAnywhere.Texts = texts;
+
+Gimmer_Core.TextAnywhere.Scene_Map_prototype_createAllWindows = Scene_Map.prototype.createAllWindows;
+Scene_Map.prototype.createAllWindows = function(){
+    this.addTextOverlay();
+    Gimmer_Core.TextAnywhere.Scene_Map_prototype_createAllWindows.call(this);
+}
+
+Scene_Map.prototype.addTextOverlay = function(){
+    let temp = new Window_Base();
+    let padding = temp.standardPadding();
+    this._textOverLay = new Window_Plain(-padding,-padding,Graphics.width,Graphics.height);
+    this.addChild(this._textOverLay);
+}
+
+Gimmer_Core.TextAnywhere._Scene_Map_prototype_update = Scene_Map.prototype.update;
+Scene_Map.prototype.update = function(){
+    Gimmer_Core.TextAnywhere._Scene_Map_prototype_update.call(this);
+    this.updateTextOverlay();
+}
+
+Scene_Map.prototype.updateTextOverlay = function(){
+    this._textOverLay.contents.clear();
+    Gimmer_Core.TextAnywhere.Texts.forEach(function(text){
+        let tempText = this._textOverLay.convertEscapeCharacters(text.text);
+        let maxWidth = this._textOverLay.contents.measureTextWidth(text)
+        this._textOverLay.contents.fontBold = text.bold;
+        this._textOverLay.contents.fontSize = text.fontsize;
+        this._textOverLay.contents.textColor = text.color;
+        this._textOverLay.contents.drawText(tempText, eval(text.x), eval(text.y), maxWidth, this._textOverLay.lineHeight());
+        this._textOverLay.resetFontSettings();
+    }, this)
+}
+
+Gimmer_Core.pluginCommands["HideTextLayer"] = function(){
+    if('_textOverLay' in SceneManager._scene){
+        SceneManager._scene._textOverLay.visible = false;
+    }
+}
+
+Gimmer_Core.pluginCommands["ShowTextLayer"] = function(){
+    if('_textOverLay' in SceneManager._scene){
+        SceneManager._scene._textOverLay.visible = show;
+    }
+}
---
+++ Plugins/Gimmer_TextAnywhere/README.md
@@ -0,0 +1,22 @@
+## Gimmer_TextAnywhere
+
+Fill in an array of text variables, and that text will show anywhere on the screen.
+Has all the same replaces as the base text (like \V for variable, \N for actor name, and \G for currency unit)
+
+### Text Tutorial:
+
+You will require the following items:
+* js/plugins: Gimmer_Core for the scaffolding needed to run this.
+* js/plugins: Gimmer_TextAnywhere
+
+In the plugin menu, go into the texts variable, and use the built in structure to provide X, Y, text, color, and fontsize variables
+
+Text will show continuously on the map screen.
+
+Want to hide text?
+
+Plugin Command HideTextLayer
+
+Want to show the text again?
+
+Plugin Command ShowTextLayer
