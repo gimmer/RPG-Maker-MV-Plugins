ca47f7d1750f01e938f0282118a235458d7ad582
Graeme <grassenti@gmail.com>
2022-12-16 10:21:07 -0500

    Adding Gimmer_YaBoring for MZ

---
+++ Plugins/MZ/Gimmer_YaBoring/Gimmer_YaBoring.js
@@ -0,0 +1,111 @@
+var Gimmer_Core = Gimmer_Core || {};
+Gimmer_Core.bore = {'loaded':true}
+
+//=============================================================================
+// Ya Boring
+//=============================================================================
+
+/*:
+ * @target MZ
+ * @plugindesc Walk away from boring events
+ * @author Gimmer_
+ *
+ * @help Gimmer_YaBoring.js
+ *
+ * Version 1.0
+ *
+ * For any event that contains only dialogue, the player is free to walk away. Doing so will end the event.
+ *
+ * @param Distance Before Closing
+ * @text Distance Before Closing
+ * @desc Number of grid positions you can walk from the event before it closes
+ * @type Number
+ * @default 3
+ *
+ */
+
+
+Gimmer_Core.bore.DISTANCE_THRESHOLD = Number(PluginManager.parameters('Gimmer_YaBoring')['Distance Before Closing']);;
+
+/**
+ * =======================
+ * Game Interpreter Mods
+ * =======================
+ */
+
+Gimmer_Core.bore.Game_Interpreter_prototype_initialize = Game_Interpreter.prototype.initialize;
+Game_Interpreter.prototype.initialize = function (depth){
+    Gimmer_Core.bore.Game_Interpreter_prototype_initialize.call(this, depth);
+    this._eventBoring = false;
+}
+
+Gimmer_Core.bore.Game_Interpreter_prototype_clear = Game_Interpreter.prototype.clear;
+Game_Interpreter.prototype.clear = function() {
+    Gimmer_Core.bore.Game_Interpreter_prototype_clear.call(this);
+    this._eventBoring = false;
+}
+
+Gimmer_Core.bore.Game_Interpreter_prototype_setup = Game_Interpreter.prototype.setup;
+Game_Interpreter.prototype.setup = function (list, eventId){
+    Gimmer_Core.bore.Game_Interpreter_prototype_setup.call(this, list, eventId);
+    this.determineIfYaBoring();
+}
+/**
+ * Checks to see if a given events page only consists of dialogue, with no switches or changes.
+ * If the event is boring, flip the flag
+ */
+Game_Interpreter.prototype.determineIfYaBoring = function(){
+    let boring = true;
+    if(this._list && this._list.length){
+        this._list.forEach((item) => {
+            if(item.code !== 101 && item.code !== 401 && item.code !==0){
+                boring = false;
+            }
+        })
+    }
+    else{
+        boring = false;
+    }
+    this._eventBoring = boring;
+}
+
+Game_Interpreter.prototype.isEventBoring = function(){
+    return this._eventBoring;
+}
+
+Game_Interpreter.prototype.didPlayerWanderOff = function(){
+    if(this._eventId > -1){
+        const event = $gameMap.event(this._eventId);
+        const distance = $gameMap.distance($gamePlayer.x, $gamePlayer.y, event.x, event.y);
+        if(distance > Gimmer_Core.bore.DISTANCE_THRESHOLD){
+            return true;
+        }
+    }
+    return false;
+}
+
+Gimmer_Core.bore.Game_Interpreter_prototype_update = Game_Interpreter.prototype.update
+Game_Interpreter.prototype.update = function (){
+    if(SceneManager._scene.constructor === Scene_Map && this.isRunning() && this.isEventBoring() && this.didPlayerWanderOff()){
+        this.clear();
+        $gameMessage.clear();
+        SceneManager._scene._messageWindow.close();
+        SceneManager._scene._messageWindow.pause = false;
+    }
+    Gimmer_Core.bore.Game_Interpreter_prototype_update.call(this);
+}
+
+/**
+ * =========================
+ * Game_Player Mods
+ * =========================
+ */
+
+Gimmer_Core.bore.Game_Player_prototype_canMove = Game_Player.prototype.canMove;
+Game_Player.prototype.canMove = function(){
+    if (($gameMap.isEventRunning() || $gameMessage.isBusy()) && $gameMap._interpreter.isEventBoring()) {
+        return true;
+    }
+
+    return Gimmer_Core.bore.Game_Player_prototype_canMove.call(this);
+}
---
+++ Plugins/MZ/Gimmer_YaBoring/README.md
@@ -0,0 +1,10 @@
+## Gimmer_YaBoring
+
+Allows any events with only dialogue in them to be walked away from and cancelled.
+Now you can have random flavour text NPCs but not players can leave if they want
+
+#### Text Tutorial:
+
+Just make events with only text (no choices, no switches, just text), and walk away.
+
+Plugin parameters let you choose how far the player can wander before the text box will close
